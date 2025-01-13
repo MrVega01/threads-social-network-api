7f630fdc53d7bdf89c3c9f33cdea4c3c4c48e9f8
VegaFogata <jose.vega@fogatagroup.com>
2025-01-13 11:56:36 -0400

    Add updateThread and deleteThread routes to threads API

--- src/routes/threads.js
+++ src/routes/threads.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import getAllThreads from '../controllers/threads/getAllThreads.js'
 import createThread from '../controllers/threads/createThread.js'
+import updateThread from '../controllers/threads/updateThread.js'
+import deleteThread from '../controllers/threads/deleteThread.js'
 
 const router = express.Router()
 
 router.get('/', getAllThreads)
 router.post('/', createThread)
-// router.delete('/', )
+router.put('/:id', updateThread)
+router.delete('/:id', deleteThread)
 
 export default router
